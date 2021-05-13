495a9077bd6c2ea9e6b4dcd953a286a5d3db6881
Marius Scherff <marius.scherff@scherff-services.de>
2021-05-13 17:20:38 +0200

    go

--- public/app.js
+++ public/app.js
@@ -4,3 +4,8 @@ const links = document.querySelector('.links')
 navToggle.addEventListener('click', function () {
   links.classList.toggle('show-links')
 })
+
+
+app.listen(5000,()=>{
+  console.log('server is listening on port 5000...')
+})
