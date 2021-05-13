44665b6580ed4559912ac39bb628572f0d6cff88
Marius Scherff <marius.scherff@scherff-services.de>
2021-05-13 17:51:26 +0200

    go

--- public/app.js
+++ public/app.js
@@ -1,11 +1,17 @@
-const navToggle = document.querySelector('.nav-toggle')
-const links = document.querySelector('.links')
+const express = require('express')
+const path = require('path')
+const app = express()
 
-navToggle.addEventListener('click', function () {
-  links.classList.toggle('show-links')
+app.use(express.static('./public'))
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.resolve(__dirname),'./navbar-app/index.html')
 })
 
+app.all('*', (req, res)=>{
+    res.status(404).send('resource not found')
+})
 
-app.listen(5000,()=>{
-  console.log('server is listening on port 5000...')
+app.listen(5000, ()=> {
+    console.log('Server is listening on Port 5000')
 })
