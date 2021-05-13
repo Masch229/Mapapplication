64867c4588999a8fdb0ee0a209ce4d2eb4453cdd
Marius Scherff <marius.scherff@scherff-services.de>
2021-05-13 19:50:22 +0200

    go

--- app.js
+++ app.js
@@ -1,30 +1,50 @@
-const express = require('express')
-const path = require('path')
-const app = express()
-const multer = require('multer');
-const upload = multer({dest: __dirname + './uploads/images'});
+const express = require("express");
+const path = require("path");
+const app = express();
+const multer = require("multer");
+const upload = multer({ dest: __dirname + "./uploads/images" });
+const app2 = express();
+const PORT = 3000;
 
-app.use(express.static('./public'))
+app.use(express.static("./public"));
 
-app.get('/',(req,res)=>{
-    res.sendFile(path.resolve(__dirname),'./navbar-app/index.html')
-})
+//app.get('/',(req,res)=>{
+//   res.sendFile(path.resolve(__dirname),'./navbar-app/index.html')
+//})
 
-app.all('*', (req, res)=>{
-    res.status(404).send('resource not found')
-})
+//app.all('*', (req, res)=>{
+//    res.status(404).send('resource not found')
+//})
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.post('/uploads', upload.single('photo'), (req, res) => {
-    if(req.file) {
-        res.json(req.file);
-    }
-    else throw 'error';
+app.get("/map", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public", "/allinone.html"));
 });
 
+app.post("/upload", upload.single("photo"), (req, res) => {
+  if (req.file) {
+    res.json(req.file);
+  } else throw "error";
+});
 
+app.listen(PORT, () => {
+  console.log("Listening at " + PORT);
+});
 
-app.listen(5000, ()=> {
-    console.log('Server is listening on Port 5000')
-})
+app.get("/uploaded", function (req, res) {
+  res.sendFile("/upload");
+});
+
+//var map = new H.Map(
+//  document.getElementById("mapContainer"),
+//  defaultLayers.vector.normal.map,
+// {
+//    zoom: 10,
+//    center: { lat: 48.81, lng: 9.01 },
+//  }
+//);
+
+app.listen(5000, () => {
+  console.log("Server is listening on Port 5000");
+});
