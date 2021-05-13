c4e71d981cce22e70fd5af41ff655221929b7a65
Marius Scherff <marius.scherff@scherff-services.de>
2021-05-13 22:32:32 +0200

    go

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
- import H from '@here/maps-api-for-javascript';
-
- // Initialize the platform object:
- var platform = new H.service.Platform({
- 'apikey': '{YOUR_API_KEY}'
- });
-
- // Obtain the default map types from the platform object
- var maptypes = platform.createDefaultLayers();
-
- // Instantiate (and display) a map object:
- var map = new H.Map(
- document.getElementById('mapContainer'),
- maptypes.vector.normal.map,
- {
-     zoom: 10,
-     center: { lng: 9.01, lat: 48.81 }
- });
+import(/* webpackChunkName: "mapsjs" */ '@here/maps-api-for-javascript').then(
+    ({ default: H }) => {
+        var platform = new H.service.Platform({
+        'apikey': '{2vrpDEAl6OdUnsU44hp2}'
+        });
+   
+        // Obtain the default map types from the platform object
+        var maptypes = platform.createDefaultLayers();
+   
+        // Instantiate (and display) a map object:
+        var map = new H.Map(
+        document.getElementById('mapContainer'),
+        maptypes.vector.normal.map,
+        {
+            zoom: 10,
+            center: { lng: 13.4, lat: 52.51 }
+        });
+    }
+    ).catch(error => console.log(error));
+   
